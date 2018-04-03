1c54e973b87be216866ac2933e95ddc01e0aff06
Julio-Maldonado <32426196+Julio-Maldonado@users.noreply.github.com>
2018-04-03 11:47:08 -0500

    Testing heroku 2

---
+++ home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset=utf-8 />
+	<title>Checkr Exercise Solutionn</title>
+</head>
+<body>
+	<form action="index.php">
+		<input type="text" name="state"/>
+		<button type="submit">Submit</button>
+	</form>
+	<br/>
+</body>
+</html>
