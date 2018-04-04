2755ebe055f038ed60b110a7fde45ec30671b887
juliom72@tamu.edu <juliom72@tamu.edu>
2018-04-04 15:59:39 -0500

    Filtering by dates implemented

--- displayData.php
+++ displayData.php
@@ -18,19 +18,49 @@ function get_data_for_graph(&$arr,$key) {
 	}
 	return $a;
 }
+/* Iterates through arra and adds to a temp array so long as the $keys are in range of 
+   $first and $second */
+function get_data_in_range(&$arr,$first,$second) {
+	$temp_arr = [];
+	foreach($arr as $key => $item) {
+		if ($key >= $first)
+			$temp_arr[$key] = $item;
+		if ($key >= $second)
+			break;
+	}
+	return $temp_arr;
+}
 
+// Get data for state clicked
 if(!empty($_GET['state'])) {
 	$data = get_data_for_state($_GET['state']);
-	$year_graph_arr = get_data_for_graph($data,'fyDeclared');	
+	$year_graph_arr = get_data_for_graph($data,'fyDeclared');
 } else {
 	$year_graph_arr = [];
+}
+
+// Used to filter data as requested by user
+if (empty($_GET['flag']))
+	$flag = false;
+else {
+	$flag = $_GET['flag'];
+	$first = $_POST['first'];
+	$second = $_POST['second'];
+	// Assert that $first is less than $second
+	if ($first > $second) {
+		$temp = $first;
+		$first = $second;
+		$second = $temp;
+	}
+	$year_graph_arr = get_data_in_range($year_graph_arr,$first,$second);
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Data for Selected State</title>
+	<?php echo '<title>Data for '.$_GET['state'].'</title>'?>
+	<?php echo '<h3>Graph for '.$_GET['state'].'</h3>'?>
 	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 	<script type="text/javascript">
 	    google.charts.load('current', {'packages':['line']});
@@ -48,9 +78,6 @@ if(!empty($_GET['state'])) {
 	        	?>
 	        ]);         
 	        var options = {
-	            chart: {
-			        title: 'Graph of Selected State',
-			    },
 			    width: 900,
 			    height: 500,
 			    axes: {
@@ -68,5 +95,18 @@ if(!empty($_GET['state'])) {
 <body>
 	<option value="decimal"></option>
 	<div id="line_top_x"></div>
+	<?php 
+	if (!$flag) {
+		echo '<p>Please input two numbers in the range of '.reset(array_keys($year_graph_arr)).' and '.end(array_keys($year_graph_arr)).'</p>';
+		echo '
+		<form name="reloadGraph" action="displayData.php?state=TX&flag=true" onsubmit="return reloadGraph()" method="post">';
+		echo '<input type="number" name="first" min = "'.reset(array_keys($year_graph_arr)).'" max = "'.end(array_keys($year_graph_arr)).'" value required>';
+		echo '<input type="number" name="second" min = "'.reset(array_keys($year_graph_arr)).'" max = "'.end(array_keys($year_graph_arr)).'" value required>';
+		echo '<input type="submit" value="Submit" >';
+		echo '</form>';
+	} else {
+		echo '<p>Please go back to modify data again</p>';
+	}
+	?>
 </body>
 </html>
